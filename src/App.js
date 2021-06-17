327a1a54e17fb8c18cabcac3758ca11d0ac6118f
senvion <senvion@senvions-MacBook-Pro.local>
2021-06-17 15:39:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import List from './List';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <div className="container">
+    <div className="row">
+      <div className="col-6 justify-content-center my-5">
+        <List/>
+      </div>
     </div>
-  );
-}
+  </div>
+);
+
 
 export default App;
